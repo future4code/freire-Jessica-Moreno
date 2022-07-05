10a2c588e18e545ed614990b068cfb335ecf6258
lilamoreno <lila.jlgm@gmail.com>
2022-07-05 17:23:27 -0300

    Exercicio useEffect

---
+++ modulo3/use-effects/src/components/PokeCard/PokeCard.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+
+function PokeCard({ pokeName }) {
+  const [pokemon, setPokemon] = useState({});
+
+  const pegaPokemon = (pokeName) => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+      .then((response) => {
+        // guarda as infos do pokemon no estado
+        setPokemon(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    pegaPokemon(pokeName);
+  }, [pokeName]);
+
+  return (
+    <div>
+      <p>{pokemon.name}</p>
+      <p>{pokemon.weight} Kg</p>
+      {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+      {pokemon.sprites && (
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      )}
+    </div>
+  );
+}
+
+export default PokeCard;
