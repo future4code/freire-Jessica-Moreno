8ecffb669a919591208598bf35233cf6050b5fb7
lilamoreno <lila.jlgm@gmail.com>
2022-07-10 19:01:21 -0300

    fiz algumas funções

--- modulo3/projeto-astromatch/src/components/Cards.js
+++ modulo3/projeto-astromatch/src/components/Cards.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import './Cards.css'
 
 
 
 function Cards () {
+    const [profile, setProfile] = useState ({})
+
+    
+    const getAProfileToChoose = async () => {
+        const url = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/jessica-moreno-freire/person"
+        try{
+            const res = await axios.get (url)
+                setProfile(res.data.profile)
+            console.log(res.data.profile)
+        } catch(err){
+            console.log(err.response.data)
+        }
+    
+    }
+    useEffect(()=>{
+        getAProfileToChoose();
+    },[])
+
+    const choosePerson = () => {
+        const url = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/jessica-moreno-freire/choose-person"
+        const body = {
+            id:profile.id,
+            choice:true
+        }
+        
+        axios.post (url,body)
+        .then((res)=>{
+            getAProfileToChoose()
+        })
+        .catch((err)=>{
+            console.log(err.data.message)
+        })
+        
+    }
+
+
     return(
-        <main>Foto</main>
+        <main>
+            <img src={profile.photo}/>
+            <p>{profile.name}, {profile.age}</p>
+            <p>{profile.bio}</p>
+            <footer><button onClick={()=> choosePerson(false) }>(x)</button> <button onClick={()=>choosePerson()}>(s2)</button>
+        </footer>
+        </main>
+        
         
     )
 
--- modulo3/projeto-astromatch/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Footer (){
-    return(
-        <footer>(x) (s2)</footer>
-    )
-}
-export default Footer
