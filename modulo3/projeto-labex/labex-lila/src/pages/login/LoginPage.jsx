2b0de93f71287908726085552a6210e6bbba2b3d
lilamoreno <lila.jlgm@gmail.com>
2022-07-13 18:12:48 -0300

    comecei a criar uns inputs

--- modulo3/projeto-labex/labex-lila/src/pages/login/LoginPage.jsx
+++ modulo3/projeto-labex/labex-lila/src/pages/login/LoginPage.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function LoginPage () {
+    
     const navigate = useNavigate()
     return (
         <div>
             <h1>login</h1>
+            <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email"
+                value={""}
+                onChange=""
+                required
+            />
+            <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Senha"
+                value={""}
+                onChange=""
+                required
+            />
+            <button>Entrar</button>
+
         </div>
     )
 }
