e7261a8786283b13a07ba52b795f008397581b07
lilamoreno <lila.jlgm@gmail.com>
2022-07-26 20:28:30 -0300

    "criando as routes"

---
+++ modulo3/projeto-labeddit/src/router/Router.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "../pages/LoginPage";
+import { CadastrePage } from "../pages/CadastrePage";
+import { FeedPage } from "../pages/FeedPage";
+import { PostPage } from "../pages/PostPage";
+
+export const Router = () =>{
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<LoginPage/>}/>
+                <Route path={"/cadastre"} element={<CadastrePage/>}/>
+                <Route path={"/feed"} element={<FeedPage/>}/>
+                <Route path={"/post/:id"} element={<PostPage/>}/>
+                
+            </Routes>
+        </BrowserRouter>
+    )
+}
