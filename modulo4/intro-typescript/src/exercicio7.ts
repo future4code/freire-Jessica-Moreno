cf4e67f42eeb143a699f6e74e1bc055a16688b5f
lilamoreno <lila.jlgm@gmail.com>
2022-08-11 18:39:51 -0300

    exercicios introdução typescript

---
+++ modulo4/intro-typescript/src/exercicio7.ts
@@ -0,0 +1,12 @@
+const dna: string = "ATTGCTGCGCATTAACGACGCGTA"
+const chars: object = {'A':'U','T':'A','C':'G','G':'C'};
+function rna (dna: string): any{
+
+    dna.replace('A', 'U')
+    .replace('T', 'A')
+    .replace('C', 'G')
+    .replace('')
+
+    return "string"
+
+}
