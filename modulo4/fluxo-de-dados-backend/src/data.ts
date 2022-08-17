918f9d62a40eaad717ae25ca33cb33f4587b8314
lilamoreno <lila.jlgm@gmail.com>
2022-08-17 16:10:55 -0300

    Exercicio de fluxo de daros

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        id:"1",
+        name:"Doritos",
+        price: 10
+    },
+    {
+        id:"2",
+        name:"Fandangos",
+        price: 8
+    },
+    {
+        id:"3",
+        name:"Cheetos",
+        price: 9
+    }
+]
