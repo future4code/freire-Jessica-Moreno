549bce88f8412c361c08d90904835bf77dd576aa
lilamoreno <lila.jlgm@gmail.com>
2022-08-18 16:53:51 -0300

    ex 4 feito

--- modulo4/fluxo-de-dados-backend/src/index.ts
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-
 import { AddressInfo } from "net";
+import { products } from "./data";
 
 const app = express();
 
@@ -15,8 +15,37 @@ const server = app.listen(process.env.PORT || 3003, () => {
   }
 });;
 
+//Exercicio 1
+
 app.get("/test", (req, res) => {
     res.send('funciona')
 })
 
-app.post("")
+//Exercicio 3
+
+app.post('/products', (req, res) =>{
+    const newProductsList = products
+    const {name, price} = req.body
+
+    const addProduct = {
+        id: Date.now().toString(),
+        name: name,
+        price: price
+    }
+
+    newProductsList.push(addProduct)
+    res.status(200).send(newProductsList)   
+})
+
+//Exercicio 4
+app.get('/products',(req,res)=>{
+    const newProductsList = products
+
+    res.status(200).send(newProductsList)
+})
+
+//Exercicio 5
+
+app.put('/product:id', (req, res) =>{
+    const id = req.params.id
+})
