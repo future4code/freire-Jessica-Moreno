7cc4eabe7c8a84e990b1496b69e2c659fc56f597
lilamoreno <lila.jlgm@gmail.com>
2022-10-13 11:24:27 -0300

    arrumando uma estilização

--- modulo6/rodada-cases-2/projeto-ambuzlnz-frontend/src/components/ModalSubmit.js
+++ modulo6/rodada-cases-2/projeto-ambuzlnz-frontend/src/components/ModalSubmit.js
@@ -1,19 +1,22 @@
+import { Button } from "@mui/material"
 import styled from "styled-components"
 
 export const ModalDiv = styled.div`
-    border: 1px solid black;
+    
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     padding: 2em;
-    background-color: white;
+    background-color: #F2F2F2;
     
     .closeSubmit {
         position: relative;
         top: -1em;
-        left: 100%;
-        background-color: blue;
+        left: 90%;
+        font-weight: bold;
+        
+        
     }
 `
 export function ModalSubmit(props) {
@@ -21,12 +24,12 @@ export function ModalSubmit(props) {
 
     return (
         <ModalDiv>
-            <button
+            <Button
                 className="closeSubmit"
                 onClick={closeSubmit}
             >
                 X
-            </button>
+            </Button>
             <h2>Pedido realizado com sucesso!</h2>
             <h3>Resumo do pedido</h3>
             <p>ID do pedido: {order.id}</p>
